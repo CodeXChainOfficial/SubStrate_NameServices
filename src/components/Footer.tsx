0e102b41f71d46a2697f8e9e62bafdc1dd71bf1c
Argoo Argo <argooargo@Argoos-MacBook-Air.local>
2023-08-28 17:05:35 +0700

    all

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -16,21 +16,12 @@ export default function Footer({ className }: FooterProps) {
         </div>
         <div>
           <span>
-            <Link to="/privacy-policy">Privacy Policy</Link>
+            <Link to="">CodeX Copyright 2023</Link>
           </span>
         </div>
       </div>
       <div className="footer-grow flex-grow-1" />
-      <div>
-        <strong>Questions?</strong>&nbsp;
-        <a
-          href="https://support.polkadot.network/"
-          rel="noreferrer"
-          target="_blank"
-        >
-          Visit our support page.
-        </a>
-      </div>
+     
     </footer>
   );
 }
